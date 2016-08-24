3650ae86204ab2a11b8ad64d4b23a6a962f83e22
hermesian <ohno.yuri@jp.fujitsu.com>
2016-08-24 18:22:05 +0900

    fix response json and initialization.

--- README.rst
+++ README.rst
@@ -39,19 +39,23 @@ You need to prepare test picture. I used `Caltech 101 <http://www.vision.caltech
 
   $ wget -O - http://www.vision.caltech.edu/Image_Datasets/Caltech101/101_ObjectCategories.tar.gz | tar xfz -
   $ curl --form "image=@101_ObjectCategories/airplanes/image_0001.jpg" http://(YOUR_HOST_IP_ADDRESS)/classify
-  {
-    "resutl": [
-      [
-        0.952552318572998,
-        "n02690373 airliner"
-      ],
-      [
-        0.02736411802470684,
-        "n04552348 warplane, military plane"
-      ],
-      [
-        0.004652403295040131,
-        "n04008634 projectile, missile"
-      ]
-    ]
-  }
+
+
+        {
+          "result": [
+              {
+                 "name": "n02690373 airliner",
+                 "score": "0.952552318573"
+              },
+              {
+                 "name": "n04552348 warplane, military plane",
+                 "score": "0.0273641180247"
+              },
+              { 
+                 "name": "n04008634 projectile, missile",
+                 "score": "0.00465240329504"
+              }
+           ] 
+         }
+
+
--- docker/grepy_cpu/inference/server.py
+++ docker/grepy_cpu/inference/server.py
@@ -1,14 +1,15 @@
 """
-classify.py is an out-of-the-box image classifer callable from the command line.
-By default it configures and runs the Caffe reference ImageNet model.
+server.py
+
+This is the inference server via Caffe.
 """
 import numpy as np
 import os
 import io
-import sys
 import time
 import ConfigParser
 
+from flask import g
 from flask import Flask, request
 from flask import jsonify
 
@@ -16,60 +17,80 @@ import caffe
 
 app = Flask(__name__)
 
-def initialize():
+
+def initialize(app):
+    print("Initialize...")
+
+    ctx = app.app_context()
+    ctx.push()
+
     caffe_dir = os.environ.get("CAFFE_ROOT")
-    
+
     # read config.ini file
     with open("config.ini") as f:
         file = f.read()
         config = ConfigParser.SafeConfigParser()
         config.readfp(io.BytesIO(file))
-    
+
     # Set parameter
-    model_def = os.path.join(caffe_dir,config.get('option','model_def'))
-    pretrained_model = os.path.join(caffe_dir,config.get('option','pretrained_model'))
-    mean_file = os.path.join(caffe_dir,config.get('option','mean_file'))
-    categories = np.loadtxt(os.path.join(caffe_dir,
-                                         "data/ilsvrc12/synset_words.txt"),str,delimiter="\t")
+    model_def = os.path.join(caffe_dir, config.get('option', 'model_def'))
+    pretrained_model = os.path.join(caffe_dir, config.get('option',
+                                    'pretrained_model'))
+    mean_file = os.path.join(caffe_dir, config.get('option', 'mean_file'))
+    g.categories = np.loadtxt(os.path.join(caffe_dir,
+                              "data/ilsvrc12/synset_words.txt"),
+                              str, delimiter="\t")
     # center_only = config.get('option','center_only')
-    center_only = True
-    #input_scale = config.get('option','input_scale')
+    g.center_only = True
+    # input_scale = config.get('option','input_scale')
     input_scale = None
     # num_of_result = config.get('option','num_of_result')
-    num_of_result = 3
-    
-    images_dim = config.get('option','images_dim')
+    g.num_of_result = 3
+
+    images_dim = config.get('option', 'images_dim')
     image_dims = [int(s) for s in images_dim.split(',')]
-    
+
     # raw_scale = config.get('option','raw_scale')
     raw_scale = 255.0
-    channel_swap = config.get('option','channel_swap')
-    
+    channel_swap = config.get('option', 'channel_swap')
+
     mean, channel_swap = None, None
     if mean_file:
         mean = np.load(mean_file)
     if channel_swap:
         channel_swap = [int(s) for s in channel_swap.split(',')]
-            
+
     caffe.set_mode_cpu()
-    print("CPU mode")
-            
+
     # Make classifier.
-    classifier = caffe.Classifier(model_def, pretrained_model,
-                                  image_dims=image_dims, mean=mean,
-                                  input_scale=input_scale, raw_scale=raw_scale,
-                                  channel_swap=channel_swap)
+    g.classifier = caffe.Classifier(model_def, pretrained_model,
+                                    image_dims=image_dims, mean=mean,
+                                    input_scale=input_scale,
+                                    raw_scale=raw_scale,
+                                    channel_swap=channel_swap)
 
-    return classifier,categories,center_only,num_of_result 
 
-            
+def array2dict(x, keys):
+    """Convert list to dictionary.
+    """
+    x = np.asarray(x)
+
+    dict = {}
+    for i in range(len(keys)):
+        dict[keys[i]] = x[i]
+
+    return dict
+
+
 @app.route('/classify', methods=['POST'])
 def classify():
     if request.method == 'POST':
 
-        # TODO: this process should be called at onece at starting flask process
-        classifier,categories,center_only,num_of_result = initialize()
-        
+        classifier = getattr(g, 'classifier', None)
+        categories = getattr(g, 'categories', None)
+        center_only = getattr(g, 'center_only', None)
+        num_of_result = getattr(g, 'num_of_result', None)
+
         image = request.files['image']
         if image:
             # input file
@@ -82,16 +103,26 @@ def classify():
             predictions = classifier.predict(inputs, not center_only)
             print("Done in %.2f s." % (time.time() - start))
 
-            prediction = zip(predictions[0].tolist(),categories)
+            # Merge with label array
+            prediction = zip(predictions[0].tolist(), categories)
             prediction.sort(cmp=lambda x, y: cmp(x[0], y[0]), reverse=True)
 
-        else:
-          return jsonify(error="Invalid request")
+            dl = []
+            keys = ['score', 'name']
+            for i in range(len(prediction[:num_of_result])):
+                    dl.append(array2dict(prediction[:num_of_result][i], keys))
 
-    return jsonify(resutl=prediction[:num_of_result])
+        else:
+            return jsonify(error="Invalid request")
+
+    return jsonify(result=dl)
+
+
+# initialize
+initialize(app)
+
 
 if __name__ == '__main__':
 
     app.debug = True
     app.run(host='0.0.0.0')
-
